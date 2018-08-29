eba7364a7bf667cb62429ee31350547bb6552430
Camila <camipereira2111@gmail.com>
2018-08-29 10:39:06 -0300

    extrae links

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+ //permite que puedas poner solo md links y te lea el archivo JS
 
 const traer = require('./md-links.js')
 const [, , ...args] = process.argv; //es el arreglo donde se guarda lo que el usuario ponga
 // console.log(args[0]);
 // console.log(args[1]);
 
-traer.rutaAbsoluta(args[0]);
+//traer.rutaAbsoluta(args[0]);
 // console.log(args);
--- md-links.js
+++ md-links.js
@@ -1,40 +1,71 @@
 //funcion para que vuelva ruta absoluta, la ruta que entrega user
 const path = require("path");
+const fs = require('fs');
 
 
+const rutaLista = path.resolve("./README.md");
+console.log(rutaLista);
+
+fs.readFile(rutaLista, 'utf-8', (err, data) => {
+    if (err) throw err;
+    markdownLinkExtractor(data);
+    // console.log(data);
+});
 
 
-function rutaAbsoluta(ruta) {
-    return new Promise((resolve, reject) => {
-        if (ruta === "") reject("ingresa algun archivo");
-        if (resolve)
+const Marked = require('marked');
 
-            rutaLista = path.resolve(ruta);
-        console.log(rutaLista);
-        return verificar(rutaLista);
-    })
+// Función necesaria para extraer los links usando marked
+// (tomada desde biblioteca del mismo nombre y modificada para el ejercicio)
+// Recibe texto en markdown y retorna sus links en un arreglo
+function markdownLinkExtractor(markdown) {
+    const links = [];
+
+    const renderer = new Marked.Renderer();
+
+    // Taken from https://github.com/markedjs/marked/issues/1279
+    const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+
+    Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+    Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+    Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+
+    renderer.link = function(href, title, text) {
+        links.push({
+            href: href,
+            text: text,
+            title: title,
+        });
+    };
+    renderer.image = function(href, title, text) {
+        // Remove image size at the end, e.g. ' =20%x50'
+        href = href.replace(/ =\d*%?x\d*%?$/, '');
+        links.push({
+            href: href,
+            text: text,
+            title: title,
+        });
+    };
+    Marked(markdown, { renderer: renderer });
+    console.log(links);
+
+    return links;
 };
 
 
-function verificar(rutaLista) {
-    const patron = ".md";
-    const rutaVerificada = (rutaLista.substring(rutaLista.lastIndexOf("."))).toLowerCase();
-    if (rutaVerificada === patron) {
-        console.log("si, esta ok")
-    } else {
-        console.log("no, esta mal");
+// function verificar(rutaLista) {
+//     const patron = ".md";
+//     const rutaVerificada = (rutaLista.substring(rutaLista.lastIndexOf("."))).toLowerCase();
+//     if (rutaVerificada === patron) {
+//         leerArchivo(rutaVerificada);
+//         console.log("si, esta ok")
+//     } else {
+//         console.log("no, esta mal");
 
-    }
-};
+//     }
 
-function leerArchivo(rutaVerificada) {
-
-}
-
-
+// };
 
 module.exports = {
-    rutaAbsoluta,
-    suma,
-    resta
+    // rutaAbsoluta
 };
