f375a081dd8cdea8d32d067abbcf6048e04970fc
Camila <camipereira2111@gmail.com>
2018-08-31 22:16:51 -0300

    cambios en index

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ const [, , ...args] = process.argv; //es el arreglo donde se guarda lo que el us
 let options = {};
 if (args[1] === "--validate") { //para hacer funcionar el argumento validate
     options = { validate: true }
-};
-mdLinks(args[0]).then((value) => { //args en 0 es la ruta que me entregue el user, value me retorna el objeto con los datos que necesito.
-    value.forEach(linkFinal => {
-        console.log(linkFinal.linea + " : " + linkFinal.href.substring(0.50) + " " + linkFinal.text.substring(0, 50));
+
+    mdLinks(args[0]).then((value) => { //args en 0 es la ruta que me entregue el user, value me retorna el objeto con los datos que necesito.
+        value.forEach((linkFinal) => {
+            console.log(linkFinal.linea + " : " + linkFinal.href.substring(0.50) + " " + linkFinal.text.substring(0, 50));
+        })
+    }).catch((error) => {
+        console.error("Error >" + error);
     });
-})
+    // } else {
+    //     options = { validate : false }
+    //     mdLinks.mdLinks(args[0], options.then())
+}
